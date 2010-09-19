136dd155b415cb5864b338312fe8a993659c2ba2
maus <maus@animus.(none)>
2010-09-19 11:24:55 +1000

    Added logging messages to Gateway.py

--- Gateway.py
+++ Gateway.py
@@ -12,6 +12,7 @@ from twisted.words.xish.domish import Element
 from wokkel.pubsub import PubSubClient, Item
 from wokkel.client import XMPPClient
 from twisted.words.protocols.jabber import jid
+from twisted.python import log
 
     
 class Device:
@@ -26,15 +27,20 @@ class Device:
 class MQTTListener(MQTTProtocol):
     
     def connectionMade(self):
+	log.msg('MQTT Connected')
         self.connect(self.factory.service.gatewayId, keepalive=1000000)
         reactor.callLater(2, self.pingreq)
         reactor.callLater(5, self.processMessages)
     
     def pingrespReceived(self):
+	log.msg('Ping received from MQTT broker')
         reactor.callLater(2, self.pingreq)
 
     def connackReceived(self, status):
-        self.subscribe(self.factory.service.gatewayRegTopic)
+	if status == 0:
+	    self.subscribe(self.factory.service.gatewayRegTopic)
+	else:
+	    log.msg('Connecting to MQTT broker failed')
         
     def processMessages(self):
         map = dict([(v,k) for k,v in self.factory.service.topicBindings])
@@ -42,7 +48,7 @@ class MQTTListener(MQTTProtocol):
             if node in map:
                 # TODO: Possible memory leak here when messages arrive to nodes that don't have a 
                 # binding. Shouldn't happen but let's make a note of it
-                print "Publishing MQTT - Topic: %s, Message: %s" % (map[node], message)
+		log.msg('Publishing message to MQTT - Topic %s, Message %s' % (map[node], message))	
                 self.publish(map[node], message)
                 self.factory.service.xmppMessageBuffer.remove((node, message))
         reactor.callLater(5, self.processMessages)
@@ -63,6 +69,7 @@ class MQTTListener(MQTTProtocol):
             # status: statusTopicOne statusTopicTwo
             # description: descriptionTopicOne
             
+	    log.msg('Registration request received:\n %s' % message)
             map = {}
             for line in message.split('\n'):
                 k, v = line.split(': ')
@@ -88,6 +95,7 @@ class MQTTListener(MQTTProtocol):
         
         else:
             # Received a publish on an output topic
+	    log.msg('Output message received\nTopic: %s, Message%s' % (topic, message))
             self.factory.service.mqttMessageBuffer.append((topic, message))
 
 class MQTTListenerFactory(ClientFactory):
@@ -98,16 +106,18 @@ class MQTTListenerFactory(ClientFactory):
 
 class XMPPPublishSubscriber(PubSubClient):
     def connectionInitialized(self):
+	log.msg('XMPP initialized')
         PubSubClient.connectionInitialized(self)
         reactor.callLater(5, self.processMessages)
         
     def printError(self, error):
-        print error
-        
+        log.msg(str(error)) 
+
     def itemsReceived(self, event):
         for item in event.items:
             if item.name == 'item':
                 for child in item.children:
+		    log.msg('Input message received\nNode: %s, Message: %s' % (event.nodeIdentifier, str(child.toXml())))
                     self.parent.parent.xmppMessageBuffer.append((event.nodeIdentifier,
                                                                   str(child.toXml())))
                 
@@ -131,12 +141,15 @@ class XMPPPublishSubscriber(PubSubClient):
                     # TODO: make a heirachy of nodes... maybe?
                     # TODO: set their expected XML namespace to EEML's
                     # TODO: WATCH OUT FOR NAMING CONFLICTS!
+		    # TODO: log the creation or failed creation of the nodes
                     service = self.parent.parent.xmppServerJID
+		    log.msg('Creating node: %s' % node)
                     self.createNode(service, node, {'pubsub#type':''},
                                     self.parent.jid).addErrback(self.printError)
             
             # Subscribe to the input nodes
             for node in inputNodeList:
+		log.msg('Subscribing to node: %s' % node)
                 self.subscribe(self.parent.parent.xmppServerJID, node,
                      self.parent.jid).addErrback(self.printError)
         
@@ -149,6 +162,7 @@ class XMPPPublishSubscriber(PubSubClient):
         
         for topic, message in buffer:
             if topic in map:
+		log.msg('Publishing output message\nNode: %s, Message: %s' % (map[topic], message)) 
                 self.publish(self.parent.parent.xmppServerJID, map[topic], [Item(None, message)]
                              ).addErrback(self.printError)
                 
@@ -194,6 +208,8 @@ class GatewayService(Service):
         
         self.xmppClient = xmppClient
         self.xmppClient.parent = self
+
+	log.msg('Starting gateway\nGateway ID: %s\nGateway registration topic: %s\nXMPP Server: %s\n' % (gatewayId, gatewayRegTopic, xmppServerJID))
     
         
     def addDevice(self, device):
@@ -211,6 +227,7 @@ def main():
     
     reactor.connectTCP('192.168.1.150', 1883, gateway.mqttFactory)
     reactor.connectTCP('pubsub.talkr.im', 5222, gateway.xmppClient.factory)
+    log.startLogging(open('./loglog', 'w'))
     
     reactor.run()
 
